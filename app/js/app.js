b762ab8d8b315039bb313133ef171d29410a4b23
LexaSem1993 <alexsemenov1993@gmail.com>
2022-09-08 22:54:24 +0300

    Добавление аккардиона

--- app/js/app.js
+++ app/js/app.js
@@ -121,3 +121,23 @@ let burgerclose = document.querySelector('.burger-menu__close'),
 	burgerclose.addEventListener('click', function(e) {
 		menuclose.classList.remove('active');
 });
+function initAcc(elem, option){
+    document.addEventListener('click', function (e) {
+        if (!e.target.matches(elem+' .a-btn')) return;
+        else{
+            if(!e.target.parentElement.classList.contains('active')){
+                if(option==true){
+                    var elementList = document.querySelectorAll(elem+' .a-container');
+                    Array.prototype.forEach.call(elementList, function (e) {
+                        e.classList.remove('active');
+                    });
+                }            
+                e.target.parentElement.classList.add('active');
+            }else{
+                e.target.parentElement.classList.remove('active');
+            }
+        }
+    });
+}
+
+initAcc('.accordion.v1', true);
